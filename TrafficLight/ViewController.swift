8019d3c6e303dd710710694472db64c35ba3751f
Mavashkens <olmashalo94@gmail.com>
2023-03-29 17:30:41 +0300

    уменьшен код исправили констрейнты

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -16,16 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet var startButton: UIButton!
     
     private var currentLight = CurrentLight.red
-    private let lightIsOn: CGFloat = 1
-    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.cornerRadius = 10
         
-        redLight.alpha = lightIsOff
-        yellowLight.alpha = lightIsOff
-        greenLight.alpha = lightIsOff
+        redLight.alpha = 0.1
+        yellowLight.alpha = 0.1
+        greenLight.alpha = 0.1
         
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = redLight.frame.width / 2
@@ -39,16 +37,16 @@ class ViewController: UIViewController {
         
         switch currentLight {
         case .red:
-            greenLight.alpha = lightIsOff
-            redLight.alpha = lightIsOn
+            greenLight.alpha = 0.1
+            redLight.alpha = 1
             currentLight = .yellow
         case .yellow:
-            redLight.alpha = lightIsOff
-            yellowLight.alpha = lightIsOn
+            redLight.alpha = 0.1
+            yellowLight.alpha = 1
             currentLight = .green
         case .green:
-            yellowLight.alpha = lightIsOff
-            greenLight.alpha = lightIsOn
+            yellowLight.alpha = 0.1
+            greenLight.alpha = 1
             currentLight = .red
         }
     }
